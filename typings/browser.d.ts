ef38e5b685bcb98634500734423362e49c8501b5
Tom Ashworth <tgvashworth@gmail.com>
2016-03-28 22:52:35 +0100

    chore: fix scripts and typings so builds work properly

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,2 +1,3 @@
+/// <reference path="browser/ambient/es6-shim/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
 /// <reference path="browser/definitions/lodash/index.d.ts" />
