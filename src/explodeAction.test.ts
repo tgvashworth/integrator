29f442c4afcdb5afd00a7d5bb4f6930d77fe2c53
Tom Ashworth <tashworth@twitter.com>
2016-03-29 16:01:28 +0100

    feat: explodeAction recurses into action before/after

--- src/explodeAction.test.ts
+++ src/explodeAction.test.ts
@@ -14,3 +14,48 @@ test("explodeAction should return a list of pairs [action, action.run]", t => {
     <ActionPair[]>[ [a, a.run] ]
   );
 });
+
+test("explodeAction should recurse into Action#before", t => {
+  const A = createAction({ displayName: "A" });
+  const a = new A;
+  const B = createAction({
+    displayName: "B",
+    before: () => [ a ]
+  });
+  const b = new B;
+  t.same(
+    explodeAction(b),
+    <ActionPair[]>[ [a, a.run], [b, b.run] ]
+  );
+});
+
+test("explodeAction should recurse into Action#after", t => {
+  const A = createAction({ displayName: "A" });
+  const a = new A;
+  const B = createAction({
+    displayName: "B",
+    after: () => [ a ]
+  });
+  const b = new B;
+  t.same(
+    explodeAction(b),
+    <ActionPair[]>[ [b, b.run], [a, a.run] ]
+  );
+});
+
+test("explodeAction should recurse into Action#before and Action#after", t => {
+  const A = createAction({ displayName: "A" });
+  const a = new A;
+  const B = createAction({ displayName: "B" });
+  const b = new B;
+  const C = createAction({
+    displayName: "C",
+    before: () => [ a ],
+    after: () => [ b ]
+  });
+  const c = new C;
+  t.same(
+    explodeAction(c),
+    <ActionPair[]>[ [a, a.run], [c, c.run], [b, b.run] ]
+  );
+});
--- src/explodeAction.ts
+++ src/explodeAction.ts
@@ -1,9 +1,11 @@
+import { flatMap } from "lodash";
 import { Action, ActionRun } from "./createAction";
 
 export type ActionPair = [ Action<any>, ActionRun ];
 
 export default function explodeAction(action: Action<any>): ActionPair[] {
-  return [
-    [action, action.run]
-  ];
+  return []
+    .concat(flatMap(action.before(), explodeAction))
+    .concat([ [action, action.run] ])
+    .concat(flatMap(action.after(), explodeAction));
 }
