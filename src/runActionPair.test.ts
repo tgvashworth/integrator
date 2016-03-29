39e769ce00f71164661e10010fab571d3bfaf836
Tom Ashworth <tashworth@twitter.com>
2016-03-29 17:42:02 +0100

    feat: add initial implementation of runActionPair

---
+++ src/runActionPair.test.ts
@@ -0,0 +1,63 @@
+import test from "ava";
+import createAction, { Action } from "./createAction";
+import { ActionPair } from "./explodeAction";
+import runActionPair from "./runActionPair";
+
+test("runActionPair is importable", t => {
+  t.ok(runActionPair);
+});
+
+test("runActionPair runs an action", t => {
+  t.plan(1);
+  const A = createAction({
+    run: () => {
+      t.pass();
+    }
+  });
+  const a = new A;
+  return runActionPair([ a, a.run ], {});
+});
+
+test("runActionPair passes state to the action", t => {
+  t.plan(1);
+  const initialState = {};
+  const A = createAction({
+    run: state => {
+      t.is(state, initialState);
+    }
+  });
+  const a = new A;
+  return runActionPair([ a, a.run ], initialState);
+});
+
+test("runActionPair propagates returned state", t => {
+  t.plan(2);
+  const initialState = {};
+  const resultState = {};
+  const A = createAction({
+    run: state => {
+      t.is(state, initialState);
+      return resultState;
+    }
+  });
+  const a = new A;
+  return runActionPair([ a, a.run ], initialState)
+    .then(state => {
+      t.is(state, resultState);
+    });
+});
+
+test("runActionPair propagates initial state if nothing returned", t => {
+  t.plan(2);
+  const initialState = {};
+  const A = createAction({
+    run: state => {
+      t.is(state, initialState);
+    }
+  });
+  const a = new A;
+  return runActionPair([ a, a.run ], initialState)
+    .then(state => {
+      t.is(state, initialState);
+    });
+});
---
+++ src/runActionPair.ts
@@ -0,0 +1,21 @@
+import { flatMap } from "lodash";
+import { Action, ActionRun } from "./createAction";
+import { ActionPair } from "./explodeAction";
+
+export type ActionPair = [ Action<any>, ActionRun ];
+
+/**
+ * runActionPair runs a single action from an ActionPair [ action, action.run ]
+ */
+export default function runActionPair(
+  pair: ActionPair,
+  state: any
+): Promise<any> {
+  const [ action, run ] = pair;
+  return run.call(action, state)
+    .then(resultState => (
+      typeof resultState === "undefined"
+        ? state
+        : resultState
+    ));
+}
