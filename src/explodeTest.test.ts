507141eb3034ff38e375100bc54f64514d62deff
Tom Ashworth <tashworth@twitter.com>
2016-04-19 22:32:51 +0100

    test: more complex explodeTest test

--- src/explodeTest.test.ts
+++ src/explodeTest.test.ts
@@ -44,9 +44,7 @@ test("explodeTest explodes Goal with setup and teardown Action", t => {
       teardown: () => [ y ]
     });
     const a = new A;
-    const q = createTest("t", [
-      a
-    ]);
+    const q = createTest("t", [ a ]);
     const setupGoalPair = <GoalPair>[ a, [ x, x.run ] ];
     const teardownGoalPair = <GoalPair>[ a, [ y, y.run ] ];
     t.same(
@@ -54,6 +52,36 @@ test("explodeTest explodes Goal with setup and teardown Action", t => {
       [
         <TestPair>[ q, setupGoalPair ],
         <TestPair>[ q, teardownGoalPair ]
+      ]
+    );
+});
+test("explodeTest explodes Goals with setup and teardown Actions", t => {
+    const X = createAction();
+    const x = new X;
+    const Y = createAction();
+    const y = new Y;
+    const A = createGoal({
+      setup: () => [ x ],
+      teardown: () => [ y ]
+    });
+    const a = new A;
+    const B = createGoal({
+      setup: () => [ x ],
+      teardown: () => [ y ]
+    });
+    const b = new B;
+    const q = createTest("t", [ a, b ]);
+    const axGoalPair = <GoalPair>[ a, [ x, x.run ] ];
+    const ayGoalPair = <GoalPair>[ a, [ y, y.run ] ];
+    const bxGoalPair = <GoalPair>[ b, [ x, x.run ] ];
+    const byGoalPair = <GoalPair>[ b, [ y, y.run ] ];
+    t.same(
+      explodeTest(q),
+      [
+        <TestPair>[ q, axGoalPair ],
+        <TestPair>[ q, bxGoalPair ],
+        <TestPair>[ q, byGoalPair ],
+        <TestPair>[ q, ayGoalPair ]
       ]
     );
 });
